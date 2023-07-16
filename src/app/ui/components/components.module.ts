9179f0fd4a7a2e00211463c64598016050998077
Samed Kütahyalı <smdkthyli@gmail.com>
2023-07-16 11:16:00 +0300

    User registration form has been added.

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { BasketsModule } from './baskets/baskets.module';
 import { ProductsModule } from './products/products.module';
 import { HomeModule } from './home/home.module';
+import { RegisterModule } from './register/register.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, BasketsModule, ProductsModule, HomeModule],
+  imports: [CommonModule, BasketsModule, ProductsModule, HomeModule, RegisterModule],
 })
 export class ComponentsModule {}
