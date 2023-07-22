85ea21aed7d3ee783d54fb1703717a7ff0b1b112
Samed Kütahyalı <smdkthyli@gmail.com>
2023-07-22 18:20:14 +0300

    Token implementation has been added.

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -3,14 +3,16 @@ import { HttpClientService } from '../http-client.service';
 import { User } from 'src/app/entities/user';
 import { Create_User } from 'src/app/contracts/users/create_user';
 import { Observable, firstValueFrom } from 'rxjs';
+ import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
+import { TokenResponse } from 'src/app/contracts/token/tokenResponse';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private httpClientService: HttpClientService) {}
+  constructor(private httpClientService: HttpClientService, private toastrService: CustomToastrService) { }
 
- async create(user: User) : Promise<Create_User> {
+  async create(user: User): Promise<Create_User> {
     const observable: Observable<Create_User | User> = this.httpClientService.post<Create_User | User>({
       controller: "users"
     }, user);
@@ -18,13 +20,22 @@ export class UserService {
     return await firstValueFrom(observable) as Create_User;
   }
 
- async login(userNameOrEmail: string, password: string, callBackFuncion?: () => void) : Promise<void> {
-   const observable: any = this.httpClientService.post({
+  async login(userNameOrEmail: string, password: string, callBackFuncion?: () => void): Promise<any> {
+    const observable: Observable<any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
       controller: "users",
       action: 'login'
-    }, {userNameOrEmail, password})
+    }, { userNameOrEmail, password })
 
-    await firstValueFrom(observable);
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+    if (tokenResponse)
+      localStorage.setItem("accessToken", tokenResponse.token.accessToken);
+
+    this.toastrService.message("Kullanıcı giriş başarılı", "Başarılı", {
+      messageType: ToastrMessageType.Success,
+      position: ToastrPosition.TopRight
+    });
     callBackFuncion();
   }
 }
+
+
