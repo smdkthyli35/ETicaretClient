7d34de8f35fe6d69ad8674fd787d6599a9b43629
Samed Kütahyalı <smdkthyli@gmail.com>
2023-08-26 15:52:42 +0300

    SignalR infrastructure has been added.

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { HubUrls } from 'src/app/constansts/hub-urls';
+import { ReceiveFunctions } from 'src/app/constansts/receive-functions';
+import { SignalrService } from 'src/app/services/common/signalr.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,11 +11,14 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent extends BaseComponent implements OnInit {
-  constructor(spinner: NgxSpinnerService) {
+  constructor(spinner: NgxSpinnerService, private signalRService: SignalrService) {
     super(spinner);
+    signalRService.start(HubUrls.ProductHub);
   }
 
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallAtom);
+    this.signalRService.on(ReceiveFunctions.ProductAddedMessageReceiveFunction, message => {
+      alert(message);
+    });
   }
 }
---
+++ src/app/constansts/hub-urls.ts
@@ -0,0 +1,3 @@
+export enum HubUrls {
+    ProductHub = "https://localhost:7027/products-hub"
+}
